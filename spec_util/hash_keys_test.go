40011dddf574bc21cfa4cb7592542e9a14d0d7ee
Cole Schlesinger <cole@akitasoftware.com>
2021-04-27 23:51:13 -0700

    Add utility for computing spec hash keys
    Some IR objects (Method.Args, Method.Responses, and Data.OneOf) require that
    their map keys be hashes of the corresponding data values, e.g. map[hash(v)] =
    v.  This PR adds a utility function for recomputing hash keys.
    
    It also updates some unit test files that, it turns out, did not respect this
    invariant.

---
+++ spec_util/hash_keys_test.go
@@ -0,0 +1,29 @@
+package spec_util
+
+import (
+	"fmt"
+	"testing"
+
+	pb "github.com/akitasoftware/akita-ir/go/api_spec"
+	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/akitasoftware/akita-libs/test"
+)
+
+func TestHashKeyRewrite(t *testing.T) {
+	// We reuse test data from meld_test, because the expected witnesses
+	// cover a range of expected IR objects.
+	for _, testData := range tests {
+		fmt.Printf("COLE: %s\n", testData.name)
+		witness := test.LoadWitnessFromFileOrDile(testData.expectedWitnessFile)
+		expected := test.LoadWitnessFromFileOrDile(testData.expectedWitnessFile)
+
+		// Witnesses are just specs with a single method.
+		err := RewriteHashKeys(&pb.APISpec{
+			Methods: []*pb.Method{witness.Method},
+		})
+		assert.NoError(t, err, testData.name)
+		assert.Equal(t, proto.MarshalTextString(expected), proto.MarshalTextString(witness), testData.name)
+	}
+}
