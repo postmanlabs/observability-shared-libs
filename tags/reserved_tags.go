9d7f7eaf789971e3fc5eabbe188a85030d1d3bf7
Jed Liu <liujed@users.noreply.github.com>
2021-06-07 16:50:26 -0700

    More tags

--- tags/reserved_tags.go
+++ tags/reserved_tags.go
@@ -9,76 +9,91 @@ import (
 
 type Key = tags.Key
 
-// Identifies the source of a trace or spec. Valid values:
-//   - user - trace/spec was manually created by a user
-//   - uploaded - trace/spec was manually uploaded by a user
-//   - deployment - trace/spec from a staging or production deployment
-//   - ci - trace/spec from a CI pipeline
-const XAkitaSource Key = "x-akita-source"
+const (
+	// Identifies the source of a trace or spec. Valid values:
+	//   - user - trace/spec was manually created by a user
+	//   - uploaded - trace/spec was manually uploaded by a user
+	//   - deployment - trace/spec from a staging or production deployment
+	//   - ci - trace/spec from a CI pipeline
+	XAkitaSource Key = "x-akita-source"
 
-// The original filesystem path of an uploaded trace.
-const XAkitaTraceLocalPath Key = "x-akita-trace-local-path"
+	// The original filesystem path of an uploaded trace.
+	XAkitaTraceLocalPath Key = "x-akita-trace-local-path"
+)
 
-// == Generic CI tags =========================================================
+// Generic CI tags
+const (
+	// Identifies the CI framework from which a trace or spec was obtained (e.g.,
+	// CircleCI, Travis).
+	XAkitaCI Key = "x-akita-ci"
+)
 
-// Identifies the CI framework from which a trace or spec was obtained (e.g.,
-// CircleCI, Travis).
-const XAkitaCI Key = "x-akita-ci"
+// CircleCI tags
+const (
+	// The contents of the CIRCLE_BUILD_URL environment variable. Attached to
+	// traces and specs derived from a CircleCI job.
+	XAkitaCircleCIBuildURL Key = "x-akita-circleci-build-url"
+)
 
-// == CircleCI tags ===========================================================
+// Travis tags
+const (
+	// The contents of the TRAVIS_BUILD_WEB_URL environment variable. Attached to
+	// traces and specs derived from a Travis job.
+	XAkitaTravisBuildWebURL Key = "x-akita-travis-build-web-url"
 
-// The contents of the CIRCLE_BUILD_URL environment variable. Attached to
-// traces and specs derived from a CircleCI job.
-const XAkitaCircleCIBuildURL Key = "x-akita-circleci-build-url"
+	// The contents of the TRAVIS_JOB_WEB_URL environment variable. Attached to
+	// traces and specs derived from a Travis job.
+	XAkitaTravisJobWebURL Key = "x-akita-travis-job-web-url"
+)
 
-// == Travis tags =============================================================
+// Generic git tags
+const (
+	// Identifies the git branch from which the trace or spec was derived.
+	// Attached to traces or specs obtained from CI.
+	XAkitaGitBranch Key = "x-akita-git-branch"
 
-// The contents of the TRAVIS_BUILD_WEB_URL environment variable. Attached to
-// traces and specs derived from a Travis job.
-const XAkitaTravisBuildWebURL Key = "x-akita-travis-build-web-url"
+	// Identifies the git commit hash from which the trace or spec was derived.
+	// Attached to traces or specs obtained from CI.
+	XAkitaGitCommit Key = "x-akita-git-commit"
 
-// The contents of the TRAVIS_JOB_WEB_URL environment variable. Attached to
-// traces and specs derived from a Travis job.
-const XAkitaTravisJobWebURL Key = "x-akita-travis-job-web-url"
+	// A link to the git repository. Attached to traces or specs obtained from a
+	// pull/merge request.
+	XAkitaGitRepoURL Key = "x-akita-git-repo-url"
+)
 
-// == Generic git tags ========================================================
+// GitHub tags
+const (
+	// Identifies the GitHub organization in which the pull request was made.
+	// Attached to traces or specs obtained from a GitHub pull request.
+	AkitaGitHubOrganizationID Key = "x-akita-github-organization-id"
 
-// Identifies the git branch from which the trace or spec was derived. Attached
-// to traces or specs obtained from CI.
-const XAkitaGitBranch Key = "x-akita-git-branch"
+	// Identifies the GitHub PR number associated with the pull request. Attached
+	// to traces or specs obtained from a GitHub pull request.
+	XAkitaGitHubPR Key = "x-akita-github-pr"
 
-// Identifies the git commit hash from which the trace or spec was derived.
-// Attached to traces or specs obtained from CI.
-const XAkitaGitCommit Key = "x-akita-git-commit"
+	// A link to the GitHub pull request. Attached to traces or specs obtained
+	// from a GitHub pull request.
+	XAkitaGitHubPRURL Key = "x-akita-github-pr-url"
 
-// A link to the git repository. Attached to traces or specs obtained from a
-// pull/merge request.
-const XAkitaGitRepoURL Key = "x-akita-git-repo-url"
+	// Identifies the GitHub repository for which the pull request was made.
+	// Attached to traces or specs obtained from a GitHub pull request.
+	XAkitaGitHubRepo Key = "x-akita-github-repo"
+)
 
-// == GitHub tags =============================================================
+// GitLab tags
+const (
+	XAkitaGitLabProject Key = "x-akita-gitlab-project"
+	XAkitaGitLabMRIID   Key = "x-akita-gitlab-mr-iid"
+)
 
-// Identifies the GitHub organization in which the pull request was made.
-// Attached to traces or specs obtained from a GitHub pull request.
-const AkitaGitHubOrganizationID Key = "x-akita-github-organization-id"
+// Packet-capture tags
+const (
+	// A comma-separated list of interfaces on which packets were captured.
+	XAkitaDumpInterfacesFlag Key = "x-akita-dump-interfaces-flag"
 
-// Identifies the GitHub PR number associated with the pull request. Attached
-// to traces or specs obtained from a GitHub pull request.
-const XAkitaGitHubPR Key = "x-akita-github-pr"
-
-// A link to the GitHub pull request. Attached to traces or specs obtained
-// from a GitHub pull request.
-const XAkitaGitHubPRURL Key = "x-akita-github-pr-url"
-
-// Identifies the GitHub repository for which the pull request was made.
-// Attached to traces or specs obtained from a GitHub pull request.
-const XAkitaGitHubRepo Key = "x-akita-github-repo"
-
-// == GitLab tags =============================================================
-
-const XAkitaGitLabProject Key = "x-akita-gitlab-project"
-const XAkitaGitLabMRIID Key = "x-akita-gitlab-mr-iid"
-
-// == Methods =================================================================
+	// The packet filter given on the command line to capture packets.
+	XAkitaDumpFilterFlag Key = "x-akita-dump-filter-flag"
+)
 
 // Determines whether a key is reserved for Akita internal use.
 func IsReservedKey(k Key) bool {
