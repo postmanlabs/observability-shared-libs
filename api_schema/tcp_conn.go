1fc29bc5a80c2a566b2d615947ec3a2168f9844d
Jed Liu <liujed@users.noreply.github.com>
2021-10-15 15:37:14 -0400

    Tweaks to `TCPConnectionReport`
    Added a `Direction` field to mirror that in witnesses.
    The `EndState` field is now a string instead of an int.

--- api_schema/tcp_conn.go
+++ api_schema/tcp_conn.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/akitasoftware/akita-libs/akid"
-	"github.com/akitasoftware/akita-libs/akinet"
 )
 
 // Details about a TCP connection that was observed.
@@ -20,13 +19,29 @@ type TCPConnectionReport struct {
 	FirstObserved time.Time `json:"first_observed"`
 	LastObserved  time.Time `json:"last_observed"`
 
+	Direction NetworkDirection `json:"direction"`
+
 	// If true, source is known to have initiated the connection. Otherwise,
 	// "source" and "destination" is arbitrary.
 	InitiatorKnown bool `json:"initiator_known"`
 
 	// Whether and how the connection was closed.
-	EndState akinet.TCPConnectionEndState `json:"end_state"`
+	EndState TCPConnectionEndState `json:"end_state"`
 }
+
+// Indicates whether a TCP connection was closed, and if so, how.
+type TCPConnectionEndState string
+
+const (
+	// Neither the FIN nor RST flag was seen.
+	ConnectionOpen TCPConnectionEndState = "OPEN"
+
+	// The FIN flag was seen, but not the RST flag.
+	ConnectionClosed TCPConnectionEndState = "CLOSED"
+
+	// The RST flag was seen.
+	ConnectionReset TCPConnectionEndState = "RESET"
+)
 
 type UploadTCPConnectionReportsRequest struct {
 	ClientID akid.ClientID          `json:"client_id"`
