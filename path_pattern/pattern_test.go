bcb07ea79da209d5791d3de522b020174311cedb
Cole Schlesinger <cole@akitasoftware.com>
2022-03-10 16:16:39 -0800

    Add prefix matching to path patterns

--- path_pattern/pattern_test.go
+++ path_pattern/pattern_test.go
@@ -7,6 +7,117 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testCase struct {
+	pattern     string
+	target      string
+	expectMatch bool
+}
+
+var commonTestCases = []testCase{
+	{
+		pattern:     "/v1/{my_arg_name}",
+		target:      "/v1/foobar",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/{my_arg_name}",
+		target:      "/v1/foobar/",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/{my_arg_name}/",
+		target:      "/v1/foobar",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/{my_arg_name}",
+		target:      "/v1/{my_old_arg_name}",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/*/{my_arg_name}",
+		target:      "/v1/foo/bar",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/*/{my_arg_name}",
+		target:      "/v1/{foo_param}/bar",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/^/{my_arg_name}",
+		target:      "/v1/foo/bar",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/{my_arg_name}",
+		target:      "/{arg1}/foobar/",
+		expectMatch: false,
+	},
+	{
+		// Variable cannot match empty component.
+		pattern:     "/v1/{my_arg_name}",
+		target:      "/v1/",
+		expectMatch: false,
+	},
+	{
+		// Target does not have enough components to match.
+		pattern:     "/v1/{my_arg_name}",
+		target:      "/v1",
+		expectMatch: false,
+	},
+	{
+		pattern:     "/v1/**",
+		target:      "/v1/foo/bar/baz",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/**/baz",
+		target:      "/v1/foo/bar/baz",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/**/baz/bar",
+		target:      "/v1/foo/bar/baz",
+		expectMatch: false,
+	},
+	{
+		pattern:     "/v1/foo/;|{}/bar",
+		target:      "/v1/foo/bar/baz",
+		expectMatch: false,
+	},
+	{
+		pattern:     "/v1/foo/b.r/baz",
+		target:      "/v1/foo/bar/baz",
+		expectMatch: false,
+	},
+	{
+		pattern:     "/^",
+		target:      "/v1",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/^",
+		target:      "/v1/foo",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/^/bar",
+		target:      "/v1/foo/bar",
+		expectMatch: true,
+	},
+	{
+		pattern:     "/v1/^",
+		target:      "/v1/{arg}",
+		expectMatch: false,
+	},
+	{
+		pattern:     "/v1/^/bar",
+		target:      "/v1/{arg}/bar",
+		expectMatch: false,
+	},
+}
+
 // For patterns with trailing slashes, for which serialization/deserialization
 // is not idempotent.
 func TestParse(t *testing.T) {
@@ -85,122 +196,63 @@ func TestParseAndString(t *testing.T) {
 }
 
 func TestMatch(t *testing.T) {
-	testCases := []struct {
-		pattern     string
-		target      string
-		expectMatch bool
-	}{
-		{
-			pattern:     "/v1/{my_arg_name}",
-			target:      "/v1/foobar",
-			expectMatch: true,
-		},
+	testCases := []testCase{
 		{
 			pattern:     "/v1/{my_arg_name}",
 			target:      "/v1/foobar/x",
 			expectMatch: false,
 		},
-		{
-			pattern:     "/v1/{my_arg_name}",
-			target:      "/v1/foobar/",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/{my_arg_name}/",
-			target:      "/v1/foobar",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/{my_arg_name}",
-			target:      "/v1/{my_old_arg_name}",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/*/{my_arg_name}",
-			target:      "/v1/foo/bar",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/*/{my_arg_name}",
-			target:      "/v1/{foo_param}/bar",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/^/{my_arg_name}",
-			target:      "/v1/foo/bar",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/{my_arg_name}",
-			target:      "/{arg1}/foobar/",
-			expectMatch: false,
-		},
-		{
-			// Variable cannot match empty component.
-			pattern:     "/v1/{my_arg_name}",
-			target:      "/v1/",
-			expectMatch: false,
-		},
-		{
-			// Target does not have enough components to match.
-			pattern:     "/v1/{my_arg_name}",
-			target:      "/v1",
-			expectMatch: false,
-		},
-		{
-			pattern:     "/v1/**",
-			target:      "/v1/foo/bar/baz",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/**/baz",
-			target:      "/v1/foo/bar/baz",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/**/baz/bar",
-			target:      "/v1/foo/bar/baz",
-			expectMatch: false,
-		},
-		{
-			pattern:     "/v1/foo/;|{}/bar",
-			target:      "/v1/foo/bar/baz",
-			expectMatch: false,
-		},
-		{
-			pattern:     "/v1/foo/b.r/baz",
-			target:      "/v1/foo/bar/baz",
-			expectMatch: false,
-		},
-		{
-			pattern:     "/^",
-			target:      "/v1",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/^",
-			target:      "/v1/foo",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/^/bar",
-			target:      "/v1/foo/bar",
-			expectMatch: true,
-		},
-		{
-			pattern:     "/v1/^",
-			target:      "/v1/{arg}",
-			expectMatch: false,
-		},
-		{
-			pattern:     "/v1/^/bar",
-			target:      "/v1/{arg}/bar",
-			expectMatch: false,
-		},
 	}
+	testCases = append(testCases, commonTestCases...)
 
 	for _, c := range testCases {
 		p := Parse(c.pattern)
 		assert.Equal(t, c.expectMatch, p.Match(c.target), c.pattern+" vs "+c.target)
+	}
+}
+
+func TestPrefixMatch(t *testing.T) {
+	testCases := []testCase{
+		{
+			pattern:     "/v1/foobar",
+			target:      "/v1/foobar/x",
+			expectMatch: true,
+		},
+		{
+			pattern:     "/v1/{my_arg_name}",
+			target:      "/v1/foobar/x",
+			expectMatch: true,
+		},
+		{
+			pattern:     "/^",
+			target:      "/v1/foobar/x",
+			expectMatch: true,
+		},
+		{
+			pattern:     "/*",
+			target:      "/v1/foobar/x",
+			expectMatch: true,
+		},
+		{
+			pattern:     "/foo/",
+			target:      "/foo",
+			expectMatch: true,
+		},
+		{
+			pattern:     "/foo",
+			target:      "/foo/",
+			expectMatch: true,
+		},
+		{
+			pattern:     "/foo",
+			target:      "/foobar",
+			expectMatch: false,
+		},
+	}
+	testCases = append(testCases, commonTestCases...)
+
+	for _, c := range testCases {
+		p := Parse(c.pattern)
+		assert.Equal(t, c.expectMatch, p.PrefixMatch(c.target), c.pattern+" vs "+c.target)
 	}
 }
