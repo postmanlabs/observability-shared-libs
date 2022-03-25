afc18e679cefecc867230de557cd21e37e0798fd
Cole Schlesinger <cole@akitasoftware.com>
2022-03-25 13:01:22 -0700

    WIP: Account for direction filters in summaries
    This commit adds tests with the desired behavior for SummarizeWithFilters()
    when a direction filter is supplied.
    
    The tests should pass but do not.

--- spec_summary/summarize_test.go
+++ spec_summary/summarize_test.go
@@ -48,6 +48,92 @@ func TestSummarize(t *testing.T) {
 	assert.Equal(t, expected, Summarize(&pb.APISpec{Methods: []*pb.Method{m1}}))
 }
 
+func TestSummarizeWithResponseCodeFilter(t *testing.T) {
+	expected := &Summary{
+		Authentications: map[string]int{
+			"BASIC": 1,
+		},
+		Directions: map[string]int{
+			"request":  1,
+			"response": 1,
+		},
+		HTTPMethods: map[string]int{
+			"POST": 1,
+		},
+		Paths: map[string]int{
+			"/v1/projects/{arg3}": 1,
+		},
+		Params: map[string]int{
+			"X-My-Header": 1,
+		},
+		Properties: map[string]int{
+			"top-level-prop":       1,
+			"my-special-prop":      1,
+			"other-top-level-prop": 1,
+		},
+		ResponseCodes: map[string]int{
+			"200": 1,
+			"201": 1,
+		},
+		DataFormats: map[string]int{
+			"rfc3339": 1,
+		},
+		DataKinds: nil,
+		DataTypes: map[string]int{
+			"string": 1,
+		},
+	}
+
+	m1 := test.LoadMethodFromFileOrDie("testdata/method1.pb.txt")
+	m2 := test.LoadMethodFromFileOrDie("testdata/method2.pb.txt")
+	methods := &pb.APISpec{Methods: []*pb.Method{m1, m2}}
+	filters := map[string][]string{"response_codes": {"200"}}
+	assert.Equal(t, expected, SummarizeWithFilters(methods, filters))
+}
+
+func TestSummarizeWithDirection(t *testing.T) {
+	expected := &Summary{
+		Authentications: map[string]int{
+			"BASIC": 2,
+		},
+		Directions: map[string]int{
+			"request":  2,
+			"response": 2,
+		},
+		HTTPMethods: map[string]int{
+			"POST": 2,
+		},
+		Paths: map[string]int{
+			"/v1/projects/{arg3}": 2,
+		},
+		Params: map[string]int{
+			"X-My-Header": 2,
+		},
+		Properties: map[string]int{
+			"top-level-prop":       0,
+			"my-special-prop":      0,
+			"other-top-level-prop": 0,
+		},
+		ResponseCodes: map[string]int{
+			"200": 0,
+			"201": 0,
+		},
+		DataFormats: map[string]int{
+			"rfc3339": 0,
+		},
+		DataKinds: nil,
+		DataTypes: map[string]int{
+			"string": 0,
+		},
+	}
+
+	m1 := test.LoadMethodFromFileOrDie("testdata/method1.pb.txt")
+	m2 := test.LoadMethodFromFileOrDie("testdata/method2.pb.txt")
+	methods := &pb.APISpec{Methods: []*pb.Method{m1, m2}}
+	filters := map[string][]string{"directions": {"request"}}
+	assert.Equal(t, expected, SummarizeWithFilters(methods, filters))
+}
+
 func TestIntersect(t *testing.T) {
 	m1 := test.LoadMethodFromFileOrDie("testdata/method1.pb.txt")
 	m2 := test.LoadMethodFromFileOrDie("testdata/method1.pb.txt")
