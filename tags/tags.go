10b6ba80886d956ba3d08a31a8b63bd99555b92c
Cole <63367934+thatplguy@users.noreply.github.com>
2022-04-18 20:07:17 -0700

    Tag multisets for tagging specs with versions (#126)
    This PR extends tags to support multiple values per tag.  Many APIs will still
    reject requests with tag multisets, but responses may now contain tag
    multisets.

--- tags/tags.go
+++ tags/tags.go
@@ -8,13 +8,36 @@ import (
 )
 
 type Key = tags.Key
+type Values = []string
 
-// Returns a map from parsing a list of "key=value" pairs.
+// FromPairs returns a map from parsing a list of "key=value" pairs.
 // Produces an error if any element of the list is improperly formatted,
 // or if any key is given more than once.
 // The caller must emit an appropriate warning if any keys are reserved.
 func FromPairs(pairs []string) (map[Key]string, error) {
-	results := make(map[Key]string, len(pairs))
+	multiset, err := FromPairsMultiset(pairs)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make(map[Key]string, len(multiset))
+	for k, vs := range multiset {
+		if len(vs) > 1 {
+			return nil, errors.Errorf("tag with key %s specified more than once", k)
+		}
+		for _, v := range vs {
+			results[k] = v
+		}
+	}
+
+	return results, nil
+}
+
+// FromPairsMultiset returns a map from parsing a list of "key=value" pairs.
+// Produces an error if any element of the list is improperly formatted.
+// The caller must emit an appropriate warning if any keys are reserved.
+func FromPairsMultiset(pairs []string) (map[Key]Values, error) {
+	results := make(map[Key]Values, len(pairs))
 	for _, p := range pairs {
 		parts := strings.Split(p, "=")
 		if len(parts) != 2 {
@@ -22,11 +45,7 @@ func FromPairs(pairs []string) (map[Key]string, error) {
 		}
 
 		k, v := Key(parts[0]), parts[1]
-		if _, ok := results[k]; ok {
-			return nil, errors.Errorf("tag with key %s specified more than once", k)
-		}
-
-		results[k] = v
+		results[k] = append(results[k], v)
 	}
 	return results, nil
 }
