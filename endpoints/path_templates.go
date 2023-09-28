ef7577fc79e24ce16897fe3df66036b5d536104a
Mudit Joshi <mudit.joshi@postman.com>
2023-09-29 01:00:04 +0530

    chore: pop first element from path template slice

--- endpoints/path_templates.go
+++ endpoints/path_templates.go
@@ -5,10 +5,11 @@ import "strings"
 // A path template whose parameter names have not been normalized.
 type PathTemplate string
 
-// Splits the path template into its path components. For example,
+// Splits the path template into its path components, and pops first empty string, For example,
 // "/v1/{foo}/bar".GetComponents() returns {"", "v1", "{foo}", "bar"}.
+// "/v1/{foo}/bar/".GetComponents() returns {"v1", "{foo}", "bar", ""}.
 func (p PathTemplate) GetComponents() []string {
-	return strings.Split(string(p), "/")
+	return strings.Split(string(p), "/")[1:]
 }
 
 func (p PathTemplate) String() string {
