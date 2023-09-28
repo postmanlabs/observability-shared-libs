c1e72503f279598c4302b2112f2e89fae4710e1c
Mudit Joshi <mudit.joshi@postman.com>
2023-09-29 01:01:57 +0530

    chore: typo fixe

--- endpoints/path_templates.go
+++ endpoints/path_templates.go
@@ -6,7 +6,7 @@ import "strings"
 type PathTemplate string
 
 // Splits the path template into its path components, and pops first empty string, For example,
-// "/v1/{foo}/bar".GetComponents() returns {"", "v1", "{foo}", "bar"}.
+// "/v1/{foo}/bar".GetComponents() returns {"v1", "{foo}", "bar"}.
 // "/v1/{foo}/bar/".GetComponents() returns {"v1", "{foo}", "bar", ""}.
 func (p PathTemplate) GetComponents() []string {
 	return strings.Split(string(p), "/")[1:]
