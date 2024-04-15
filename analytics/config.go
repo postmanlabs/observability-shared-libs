ff8036138dc1f02f5ed0c20f987d1399cc3ca948
Mudit Joshi <116863733+mudit-postman@users.noreply.github.com>
2024-04-15 12:28:26 +0530

    [POA-484] Remove Mixpanel and Segment libs (#211)

--- analytics/config.go
+++ analytics/config.go
@@ -1,40 +1,13 @@
 package analytics
 
 type Config struct {
-	// Toggle for additionally sending all events to Mixpanel
-	IsSegmentEnabled bool `yaml:"segment_enabled"`
-
-	// The key used to identify the Segment source to use
-	WriteKey string `yaml:"segment_write_key"`
-
-	// The endpoint to which the segment client connects to send events
-	SegmentEndpoint string `yaml:"segment_endpoint"`
-
 	App AppInfo `yaml:"app"`
 
 	// Toggle for logging sent events
 	IsLoggingEnabled bool `yaml:"logging_enabled"`
 
-	// TODO: This should be removed once we have fully migrated over to Segment
-	// Toggle for additionally sending all events to Mixpanel
-	IsMixpanelEnabled bool `yaml:"mixpanel_enabled"`
-
-	// The Mixpanel token.
-	MixpanelToken string `yaml:"mixpanel_token"`
-
-	// The Mixpanel endpoint
-	MixpanelEndpoint string `yaml:"mixpanel_endpoint"`
-
-	// If present, adds a Basic authentication header with secret as the
-	// username and the empty string as the password.
-	MixpanelSecret string `yaml:"mixpanel_secret"`
-
 	// Disable batching (used for CLI) by setting this parameter to 1
 	BatchSize int `yaml:"batch_size"`
-
-	// Sets the default destination types that events will be sent to.
-	// If not set, events will be sent to all destinations.
-	DefaultIntegrations map[string]bool `yaml:"default_integrations"`
 
 	// Toggle for sending events to Amplitude
 	IsAmplitudeEnabled bool `yaml:"amplitude_enabled"`
