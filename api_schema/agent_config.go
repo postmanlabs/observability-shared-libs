c4b18f05b25cf3872559cf7ef30c1ee5a33b4d19
Jed Liu <liujed@users.noreply.github.com>
2025-01-22 16:58:49 -0500

    POA-2742 Add types for conveying dynamic agent configuration

---
+++ api_schema/agent_config.go
@@ -0,0 +1,18 @@
+package api_schema
+
+import "regexp"
+
+// Reflects the version of the JSON encoding of ServiceAgentConfig. Increase the
+// minor version number for backwards-compatible changes, and the major version
+// number for non-backwards-compatible changes.
+const ServiceAgentConfigVersion = "v0.1"
+
+type ServiceAgentConfig struct {
+	FormatVersion  string               `json:"version"`
+	FieldsToRedact FieldRedactionConfig `json:"fields_to_redact"`
+}
+
+type FieldRedactionConfig struct {
+	FieldNames       []string         `json:"field_names"`
+	FieldNameRegexps []*regexp.Regexp `json:"field_name_regexps"`
+}
