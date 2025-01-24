2f0b46e9bbaef763bfddfb1d469fc399326685b4
Jed Liu <liujed@users.noreply.github.com>
2025-01-24 11:00:20 -0500

    Add ServiceAgentConfig constructors and cloning (#223)

--- api_schema/agent_config.go
+++ api_schema/agent_config.go
@@ -8,11 +8,49 @@ import "regexp"
 const ServiceAgentConfigVersion = "v0.1"
 
 type ServiceAgentConfig struct {
-	FormatVersion  string               `json:"version"`
-	FieldsToRedact FieldRedactionConfig `json:"fields_to_redact"`
+	FormatVersion  string                `json:"version"`
+	FieldsToRedact *FieldRedactionConfig `json:"fields_to_redact"`
 }
 
 type FieldRedactionConfig struct {
 	FieldNames       []string         `json:"field_names"`
 	FieldNameRegexps []*regexp.Regexp `json:"field_name_regexps"`
+}
+
+func NewServiceAgentConfig() *ServiceAgentConfig {
+	return &ServiceAgentConfig{
+		FormatVersion:  ServiceAgentConfigVersion,
+		FieldsToRedact: NewFieldRedactionConfig(),
+	}
+}
+
+func NewFieldRedactionConfig() *FieldRedactionConfig {
+	return &FieldRedactionConfig{
+		FieldNames:       []string{},
+		FieldNameRegexps: []*regexp.Regexp{},
+	}
+}
+
+// Returns a deep copy of this configuration.
+func (config *ServiceAgentConfig) Clone() *ServiceAgentConfig {
+	if config == nil {
+		return nil
+	}
+
+	return &ServiceAgentConfig{
+		FormatVersion:  config.FormatVersion,
+		FieldsToRedact: config.FieldsToRedact.Clone(),
+	}
+}
+
+// Returns a deep copy of this configuration.
+func (config *FieldRedactionConfig) Clone() *FieldRedactionConfig {
+	if config == nil {
+		return nil
+	}
+
+	return &FieldRedactionConfig{
+		FieldNames:       append([]string{}, config.FieldNames...),
+		FieldNameRegexps: append([]*regexp.Regexp{}, config.FieldNameRegexps...),
+	}
 }
